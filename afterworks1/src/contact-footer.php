b73cd546b11f43297a386d8d21127d0e5e9d19db
Tomohiro Ueno <cipher@TomohironoMacBook-Pro.local>
2018-08-05 14:13:13 +0900

    ファイル削除

--- afterworks1/src/contact-footer.php
+++
@@ -1,7 +0,0 @@
-﻿</section>
-<footer>
-<p class="copy"><small>&copy;&ensp;Tomohiro&ensp;Ueno,&ensp;All&ensp;rights&ensp;reserved.</small></p>
-</footer>
-</body>
-
-</html>
--- afterworks1/src/test_mail.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-if(mb_send_mail('ueno.t.humansystem@gmail.com', 'TEST SUBJECT', 'TEST BODY')){
-echo '送信完了';
-}else{
-echo '送信失敗';
-}
-?>
